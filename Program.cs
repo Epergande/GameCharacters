09adeb87eff1bff92c4b85bdf38dda4199ce0317
Epergande <elliott.pergande1@gmail.com>
2025-03-06 13:57:33 -0600

    [1.8] added menu

--- Program.cs
+++ Program.cs
@@ -13,4 +13,33 @@ List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(ma
 
 logger.Info("Program ended");
 
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Display Mario Characters");
+  Console.WriteLine("2) Add Mario Character");
+  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("Press enter to quit");
+
+  // input selection
+  string? choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+
+  if (choice == "1")
+  {
+    // Display Mario Characters
+  }
+  else if (choice == "2")
+  {
+    // Add Mario Character
+  }
+  else if (choice == "3")
+  {
+    // Remove Mario Character
+  } else if (string.IsNullOrEmpty(choice)) {
+    break;
+  } else {
+    logger.Info("Invalid choice");
+  }
+} while (true);
 
