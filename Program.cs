6745078f75d35b40187f354b8e25aaf697c6388f
Epergande <elliott.pergande1@gmail.com>
2025-03-06 14:03:39 -0600

    [2.1] added name,decsrpition

--- Program.cs
+++ Program.cs
@@ -40,7 +40,13 @@ do
     Mario mario = new()
     {
       Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
+          // Input Name, Descriptio
     };
+        // Input Name, Description
+    Console.WriteLine("Enter Name:");
+    mario.Name = Console.ReadLine();
+    Console.WriteLine("Enter Description:");
+    mario.Description = Console.ReadLine();
   }
   else if (choice == "3")
   {
